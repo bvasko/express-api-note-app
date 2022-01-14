dbd30e44af8849037b523313775926f3404a8bb0
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2022-01-14 10:06:51 -0500

    move notes api response to server.js; return notes to front-end

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const notesRouter = require('./notes');
-
-const app = express();
-
-app.use('/notes', notesRouter);
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const api = require('./routes/index.js');
 // Helper method for generating unique ids
 // const uuid = require('./helpers/uuid');
 
@@ -11,7 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -21,6 +19,14 @@ app.get('/', (req, res) =>
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes',(req, res) => {
+  console.info(`${req.method} request received for notes`);
+  fs.readFile('./db/db.json', 'utf8', (err, data)=> {
+    if (err) return console.error(err);
+    return data;
+  });
 });
 
 app.listen(PORT, () =>
